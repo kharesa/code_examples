cd9a8f787589b251be2889b348c46a677842cdfc
kharesa <43879855+kharesa@users.noreply.github.com>
2023-02-28 10:58:06 +0000

    Update lumo_distance_calc.py

--- lumo_distance_calc.py
+++ lumo_distance_calc.py
@@ -3,9 +3,10 @@ import numpy as np
 from pyproj import Transformer
 
 # read in xlsx workbook sheet 'Distance_Matrix_Concat_Lumo' file into a dataframe
-df = pd.read_excel('filename.xlsx', sheet_name='Distance_Matrix_Concat_Lumo')
-df_transit = pd.Dataframe(columns=['route','OLat','OLon','DLat','DLon','mode'])
-df_road = pd.Dataframe(columns=['route','OLat','OLon','DLat','DLon','mode'])
+df = pd.read_excel('230203 First Rail_Lumo Distances.xlsx', sheet_name='Distance_Matrix_Concat_Lumo')
+#prepping output dfs
+df_transit = pd.DataFrame(columns=['route','OLat','OLon','DLat','DLon','mode'])
+df_road = pd.DataFrame(columns=['route','OLat','OLon','DLat','DLon','mode'])
 
 # define function to check for missing values and data types of each column
 def check_missing_data_types(df):
@@ -18,10 +19,20 @@ check_missing_data_types(df)
 # define function to convert Easting and Northing coordinates into latitude longitude
 def lat_lon_conversion(df):
     transformer = Transformer.from_crs('epsg:27700', 'epsg:4326') # define coordinate reference systems
-    df[['O_Lon', 'O_Lat']] = pd.DataFrame(transformer.transform(df['O_Easting'].values, df['O_Northing'].values)).T
-    df[['D_Lon', 'D_Lat']] = pd.DataFrame(transformer.transform(df['D_Easting'].values, df['D_Northing'].values)).T
-    df = df.drop(['O_Easting', 'O_Northing', 'D_Easting', 'D_Northing'], axis=1) # drop old columns
-    return df.rename(columns={'O_Lon': 'O_Lon', 'O_Lat': 'O_Lat', 'D_Lon': 'D_Lon', 'D_Lat': 'D_Lat'}) # rename new columns
+    df[['O_Easting', 'O_Northing']] = pd.DataFrame(transformer.transform(df['O_Easting'].values, df['O_Northing'].values)).T
+    df[['D_Easting', 'D_Northing']] = pd.DataFrame(transformer.transform(df['D_Easting'].values, df['D_Northing'].values)).T
+    return df.rename(columns={'O_Easting': 'OLat', 'O_Northing': 'OLon', 'D_Easting': 'DLat', 'D_Northing':'DLon'}, inplace=True) # rename new columns
 
 # execute function to convert Easting and Northing coordinates into latitude longitude and rename columns
 df = lat_lon_conversion(df)
+
+
+#loop through the dataframe
+for index, row in df.iterrows():
+
+    route = row["Concat"]
+    OLat = row["OLat"]
+    OLon = row["OLon"]
+    DLat = row["DLat"]
+    DLon = row["DLon"]
+
